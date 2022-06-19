21ce2be8d8b8e108aa899d576a6913e0a96e781c
pratikboghani1 <78678091+pratikboghani1@users.noreply.github.com>
2022-06-19 23:30:40 +0530

    First Commit

---
+++ db/db.js
@@ -0,0 +1,339 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hi &#128075;",
+				"I'm Pratik Boghani. Currently, i’m a student at Humber College, Canada. I'm specialized in building <b>Flutter Applications & C# Windows Softwares + Python Scripts.",
+				"I have completed my Bachelor in CS from Gujarat Technological University, Gujarat. I'm a developer, geek and curious human	besides being an OpenSource enthusiast. I have experience of delivering solutions for interesting problems from startup and industry space.",
+				"I love to Travel+Code+Music.",
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email at pratikboghani1@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Computer S/W Languages",
+			skillName: "C#, C++, C",
+			color: "1",
+			percentage: "90",
+		},
+		{
+			title: "Languages",
+			skillName: "Python, OpenCV, NumPy, Tkinter",
+			color: "2",
+			percentage: "75",
+		},
+		{
+			title: "Mobile App Dev Languages",
+			skillName: "Flutter, Dart, Firebase",
+			color: "4",
+			percentage: "70",
+		},
+		{
+			title: "Database",
+			skillName: "Ms-SQL Server, MySql",
+			color: "3",
+			percentage: "85",
+		},
+		{
+			title: "Design",
+			skillName: "HTML, Bootstrap, SCSS, PHP",
+			color: "5",
+			percentage: "40",
+		},
+		{
+			title: "Version Control",
+			skillName: "GitHub",
+			color: "6",
+			percentage: "70",
+		},
+		{
+			title: "Tools",
+			skillName: "Postman, PAGE,  XAMPP",
+			color: "2",
+			percentage: "75",
+		},
+		{
+			title: "Tools",
+			skillName: "Infragistics, SAP Crystal Reports, MS-Office",
+			color: "1",
+			percentage: "75",
+		},
+		{
+			title: "Editor",
+			skillName: "Visual Studio, Android Studio, VS Code",
+			color: "3",
+			percentage: "80",
+		},
+		{
+			title: "API",
+			skillName: "REST APIs, Documentation",
+			color: "4",
+			percentage: "80",
+		},
+	],
+	projects: {
+		web: [
+			{
+				projectName: "ERP Student",
+				image: "images/pERP.png",
+				summary:
+					"Developed a full stack ERP application to provide management of student's data with admin interface.",
+				preview: "https://github.com/pratikboghani/ERP-Student",
+				techStack: ["JavaScript","CSS","PHP","MySQL"],
+			},
+			{
+				projectName: "Feedback Form",
+				image: "images/pERP.png",
+				summary:
+					"Developed a .Net application to render a list of faculty member fetched from DB. Filtered the Faculties based on department and take feedback from students and generate faculty evaluation report.",
+				preview: "",
+				techStack: [".Net", "SQL Server", "CSS", "JavaScript"],
+			},
+		],
+		software: [
+			{
+				projectName: "Diamond Inventory System",
+				image: "images/PdiamonInvSys.png",
+				summary:
+					"It is a vast inventory and Diamond manufacturing process tracking system. It's a complete ERP package for Diamond manufacturing companies. DIS has been developed to incorporate all possibilities and permutations, combinations to address all needs of diamond manufacturing companies. DIS deals with all the requirement of Diamond Industries in managing very complex Diamond Manufacturing process.",
+				preview:
+					"",
+				techStack: ["c#", "SQL Server",".Net Framework","Infragistics","SAP Crystal Reports"],
+			},
+			{
+				projectName: "Face Recognisation Based Attendance System",
+				image: "images/PFaceRecAttSys.png",
+				summary:
+					"Standalone PC software to mark attendance of multiple employees at a time. There is no need to stand in front of the camera and no physical involvement. Included a website and android app to manage data.	",
+				preview: "",
+				techStack: ["Python", "OpenCV", "TKinter","PAGE","MySQL","XAMPP","NumPy","Pandas","PIL"],
+			},
+			{
+				projectName: "Textile Inventory System",
+				image: "images/pTexInvSys.jpg",
+				summary:
+					"Software to store and manage, calculate, and share textile information and generate bills. It gives prediction and full analysis of a data.",
+				preview: "",
+				techStack: ["c#", "SQL Server",".Net Framework","Infragistics","SAP Crystal Reports"],
+			},
+			{
+				projectName: "Task Control",
+				image: "images/PTaskControl.jpg",
+				summary:
+					"Software to store and manage work task assign by manager to the employees in a company. It gives reminder of work and make tracking of employee's work.",
+				preview: "",
+				techStack: ["c#", "SQL Server",".Net Framework","Infragistics","SAP Crystal Reports"],
+			},
+			{
+				projectName: "Payroll System",
+				image: "images/pPayRollSys.jpg",
+				summary:
+					"Complete payroll software with support of biometric device. Secure, scalable and maintenance-free. Make your employees smile with timely, accurate paychecks.",
+				preview: "",
+				techStack: ["c#", "SQL Server",".Net Framework","Infragistics","SAP Crystal Reports"],
+			},
+			{
+				projectName: "QR Generator, WhatsApp messages and Email sender",
+				image: "images/pQrGenerator.jpg",
+				summary:
+					"Automatic softwar that runs with computer startups in system tray. It creates QR code of .txt file and send emails or whatsapp messages",
+				preview: "",
+				techStack: ["c#",".Net Framework"],
+			},
+		],
+		android: [
+			{
+				projectName: "BMI-App",
+				image: "images/pBMI.jpg",
+				summary:
+					"The Application generates BMI reports by taking few detais of a person.",
+				preview: "",
+				techStack: ["Flutter","Dart", "Android Studio"],
+			},
+			{
+				projectName: "English Words",
+				image: "images/pEngWords.jpg",
+				summary:
+					"A learning Application that displays a list of english words in two languages: English and Gujarati.",
+				preview: "",
+				techStack: ["Flutter","Dart", "Android Studio","Google Translator"],
+			},
+			{
+				projectName: "WorkFlow",
+				image: "images/pWorkFlow.jpg",
+				summary:
+					"Employee management system application that we ever used to manage our team, It's very easy to use. Its interconnected processes will help you increase efficiency, foster collaboration, and macro-monitor performance. This app is one-stop solution for managing your entire business. This app manages the all task of all the current employees of the company.",
+				preview: "",
+				techStack: ["Flutter","Dart", "Android Studio","REST API","Firebase"],
+			},
+			{
+				projectName: "ETEX",
+				image: "images/pETEX.jpg",
+				summary:
+					"This app manage all bills in Textile Industry, Send WhatsApp messages and emails to the customers.",
+				preview: "",
+				techStack: ["Flutter","Dart", "Android Studio","REST API","Firebase"],
+			},
+		],
+		freelance: [
+			// {
+			// 	projectName: "SnylloAir.com",
+			// 	image: "images/snylloair.png",
+			// 	summary:
+			// 		"Developed a company website to showcase the purpose, services and products provided by the company to audience.",
+			// 	preview: "https://www.snylloair.com/",
+			// 	techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+			// },
+			// {
+			// 	projectName: "Delivery+",
+			// 	image: "images/AM-Logo-.png",
+			// 	summary:
+			// 		"Android Application to display website in android devices.",
+			// 	preview:
+			// 		"https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+			// 	techStack: ["Android", "JAVA", "Play Store"],
+			// },
+		],
+	},
+	experience: [
+		{
+			title: "UpWork-Freelancer",
+			duration: "December 2021 - Present",
+			subtitle: "Flutter Developer",
+			details: [
+				"Worked as a Flutter developer to make client base Android App.",
+				"Developed a customer Mobile Application from scratch for Employee Task Management and build ETEX App, which is used in Textile business.",
+			],
+			tags: [
+				"Flutter",
+				"C#",
+				"Python",
+				"Multiple Programming languages",
+				
+			],
+			icon: "group",
+		},
+		{
+			title: "Silver Technologies",
+			duration: "December 2020 - July 2021",
+			subtitle: "Software Engineer",
+			details: [
+				"Worked on Desktop Applications. APIs, SQL Server. The main projects are Diamond Inventory System, Textile Inventory System, Payroll. In which I have developed new, efficient, and well-tested code.",
+				"Crafted and Integrated new reports using SQL-Server. Coded software updates to add new features, close security holes, and resolve defects.",
+				"Co-ordinated closely with the product team, backend team, android team, and QA team to deliver the product builds before deadlines.",
+			],
+			tags: [
+				"C#",
+				"SQL-Server",
+				"Infragistics",
+				"SAP Crystal Reports",
+				"Github",
+			],
+			icon: "group",
+		},
+		{
+			title: "Bitwise Infotech",
+			duration: "May 2020 - October 2020",
+			subtitle: "Intern",
+			details: [
+				"Worked on web project and Solved various DS queries.",
+				"Integrated QR Code and reduced the effective time by 50 percent.",
+			],
+			tags: ["Python","C/C++", "MySQL", "TkInter", "OpenCV", "NumPy"],
+			icon: "group",
+		},
+	],
+	education: [
+		{
+			title: "Bachelors in Computer Engineering",
+			duration: "2017-2021",
+			subtitle: "Gujarat Technological University",
+			details: [
+				"CGPA : 9.25",
+				"WES Evaluation Score : 3.83/4",
+				"Received 500+ stars and 300+ forks on GitHub projects.",
+			],
+			tags: [
+				"Data Structures & Algorithms",
+				"Operating Systems",
+				"Database Management System",
+				"Python Programming",
+				"Machine Learning",
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "Class 11-12th in Science and Mathematics",
+			duration: "2015-2017",
+			subtitle: "Gujarat Secondary and Higher Secondary Education Board",
+			details: [
+				"Qualified JEE Main",
+				"Secured 82% in Class 12th Boards Examinations.",
+			],
+			tags: ["Physics", "Chemistry", "Mathematics"],
+			icon: "book",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				// {
+				// 	text: "Stackoverflow",
+				// 	link: "https://stackoverflow.com/users/8461233/vinay-somawat",
+				// },
+				{
+					text: "GitHub",
+					link: "https://github.com/pratikboghani",
+				},
+				// {
+				// 	text: "LeetCode",
+				// 	link: "https://leetcode.com/somawatvinay/",
+				// },
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				{
+					text: "Print this page",
+					func: "window.print()",
+				},
+				
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/pratik-boghani-2b73321b1",
+				},
+				{
+					text: "Instagram",
+					link: "https://www.instagram.com/mr.pratikboghani/",
+				},
+				{
+					text: "Buy me a coffee",
+					link: "https://www.buymeacoffee.com/pratikboghani",
+				},
+			],
+		},
+		{
+			label: "copyright-text",
+			data: [
+				"Made with &hearts; by Pratik Boghani.",
+				"&copy; No Copyrights. Feel free to use this template."
+			],
+		},
+	],
+};
